b334133bc1914822779caf040f6dd5261224939f
Chandan8585 <chandanwingshr@gmail.com>
2024-01-19 19:51:45 +0530

    search Bar UI created

--- pixabay/src/components/searchBar/SearchBar.jsx
+++ pixabay/src/components/searchBar/SearchBar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import "./searchBar.css";
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='search_container'>
+      <div className='search_input'>
+      <i className="material-icons">search</i>
+      <input type="text" placeholder='Search that you want to see' className='input'/>
+      </div>
+     <button className='search_btn'>Go !</button>
+    </div>
   )
 }
 
