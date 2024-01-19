5c052c130fbcde35d2d8e9cdfc56e7e67f0a2199
Chandan8585 <chandanwingshr@gmail.com>
2024-01-19 20:02:20 +0530

    home page created

--- pixabay/src/pages/home/Home.jsx
+++ pixabay/src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import "./home.css";
+import mainImg from "../../assets/homepage_img.png";
+import SearchBar from '../../components/searchBar/SearchBar';
 const Home = () => {
   return (
-    <div className='home'>
+    <div className='home' style={{backgroundImage: `url(${mainImg})`}}>
       <div className='navbar'>
       <Navbar/>
       </div>
+      
+      <h1 className='heading'> Discover over 2,000,000 <br />
+        free Stock Images</h1>
+        <div className='search_section'>
+        <SearchBar/>
+        </div>
+     
     </div>
   
   )
